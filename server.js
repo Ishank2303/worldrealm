dc22294d28d6e5894b5dc05b2fc60c9876dc6634
Ishank Sharma <ishanksharma2372003@gmail.com>
2024-05-20 16:36:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+var axios = require("axios");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+console.log(path.join(__dirname,'public'));
+return res.sendFile('public/index.html',{root : __dirname});
+
+})
+
+app.get("/searchword", (req, res) => {
+  
+console.log(req.query)
+
+var options = {
+  method: 'GET',
+  url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/definition/',
+  params: {entry: req.query.entry},
+  headers: {
+    'X-RapidAPI-Key': '3dc0e78068mshf6a6c2be78f257cp176981jsn7ac2c27874ea',
+    'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+res.json(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+
+// let response = {}
+// response.data = 
+//     {
+//         entry: 'calm',
+//         request: 'calm',
+//         response: 'calm',
+//         meaning: {
+//           noun: '(nou) steadiness of mind under stress\n' ,
+//           verb: '(vrb) make calm or still\n' +
+//             '(vrb) make steady\n',
+//           adverb: '',
+//           adjective: '(adj) not agitated; without losing self-possession\n' 
+//         },
+//         ipa: 'kɑːm',
+//         version: '7.5.7',
+//         author: 'twinword inc.',
+//         email: 'help@twinword.com',
+//         result_code: '200',
+//         result_msg: 'Success'
+
+// }
+
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}- http://localhost:3000`);
+});
